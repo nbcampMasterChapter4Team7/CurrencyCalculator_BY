dbf732635c8882432d1ef96714f4734691f3fff9
LEEBUYONG <dqd001@naver.com>
2025-04-16 21:02:33 +0900

    add: MVVM 폴더 변경과 기초 세팅 진행

---
+++ CurrencyCalculator_BY/CurrencyCalculator_BY/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CurrencyCalculator_BY
+//
+//  Created by iOS study on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        print("화면이 로드됨")
+    }
+}
+
